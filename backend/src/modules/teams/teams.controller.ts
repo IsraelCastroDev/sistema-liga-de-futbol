6bbdb2651c82131f6b75a15a7ab178cfda2fba35
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-11 12:14:48 -0500

    usando guards y el decorador Roles

--- backend/src/modules/teams/teams.controller.ts
+++ backend/src/modules/teams/teams.controller.ts
@@ -6,16 +6,23 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { TeamsService } from './teams.service';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/role.decorator';
+import { Role } from '../users/enums/role.enum';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
 @Controller('teams')
 export class TeamsController {
   constructor(private readonly teamsService: TeamsService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
   create(@Body() createTeamDto: CreateTeamDto) {
     return this.teamsService.create(createTeamDto);
   }
