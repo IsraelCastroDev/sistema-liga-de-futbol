94ffe6fccf9fda9eae82409b0485ec03af8f6d9a
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-11 12:46:59 -0500

    protegiendo rutas de news

--- backend/src/modules/news/news.controller.ts
+++ backend/src/modules/news/news.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { NewsService } from './news.service';
+import { Roles } from '../auth/decorators/role.decorator';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 import { CreateNewsDto } from './dto/create-news.dto';
 import { UpdateNewsDto } from './dto/update-news.dto';
+import { Role } from '../users/enums/role.enum';
 
 @Controller('news')
 export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
+  @Roles(Role.ADMIN, Role.MANAGER)
+  @UseGuards(AuthGuard, RolesGuard)
   @Post()
   create(@Body() createNewsDto: CreateNewsDto) {
     return this.newsService.create(createNewsDto);
@@ -23,11 +38,15 @@ export class NewsController {
   }
 
   @Patch(':id')
+  @Roles(Role.ADMIN, Role.MANAGER)
+  @UseGuards(AuthGuard, RolesGuard)
   update(@Param('id') id: string, @Body() updateNewsDto: UpdateNewsDto) {
     return this.newsService.update(+id, updateNewsDto);
   }
 
   @Delete(':id')
+  @Roles(Role.ADMIN, Role.MANAGER)
+  @UseGuards(AuthGuard, RolesGuard)
   remove(@Param('id') id: string) {
     return this.newsService.remove(+id);
   }
--- backend/src/modules/news/news.module.ts
+++ backend/src/modules/news/news.module.ts
@@ -3,9 +3,11 @@ import { NewsService } from './news.service';
 import { NewsController } from './news.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { News } from './entities/news.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([News])],
+  imports: [TypeOrmModule.forFeature([News]), JwtModule, UsersModule],
   controllers: [NewsController],
   providers: [NewsService],
 })
