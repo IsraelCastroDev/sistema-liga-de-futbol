1ac2893d53906fcc8d99dae756f745f22499c224
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-10 22:05:10 -0500

    creando modulos player y team

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,6 +7,8 @@ import { typeOrmConfig } from './config/typeOrm.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { NewsModule } from './modules/news/news.module';
+import { TeamsModule } from './modules/teams/teams.module';
+import { PlayersModule } from './modules/players/players.module';
 
 @Module({
   imports: [
@@ -18,6 +20,8 @@ import { NewsModule } from './modules/news/news.module';
     AuthModule,
     UsersModule,
     NewsModule,
+    TeamsModule,
+    PlayersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
