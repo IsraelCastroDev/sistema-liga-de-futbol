25990349f1b49b30b3ebb65a030e15edf00d2bc0
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-10 23:14:03 -0500

    creando entidad y dto de futbolista

--- backend/src/modules/players/dto/create-player.dto.ts
+++ backend/src/modules/players/dto/create-player.dto.ts
@@ -1,1 +1,41 @@
-export class CreatePlayerDto {}
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+} from 'class-validator';
+import { DominantFoot } from '../enums/dominantFoot.enum';
+
+export class CreatePlayerDto {
+  @IsNotEmpty({ message: 'El nombre del futbolista es obligatorio' })
+  name: string;
+
+  @IsNotEmpty({ message: 'El nombre del país es obligatorio' })
+  country: string;
+
+  @IsNotEmpty({
+    message: 'La fecha de nacimiento del futbolista es obligatoria',
+  })
+  birthdate: string;
+
+  @IsNotEmpty({ message: 'La posición del futbolista es obligatoria' })
+  position: string;
+
+  @IsEnum(DominantFoot, { message: 'Pie dominante inválido' })
+  dominantFoot: DominantFoot;
+
+  @IsNumber({}, { message: 'Altura inválida, debe ser un número' })
+  height: number;
+
+  @IsNumber({}, { message: 'Peso inválido, debe ser un número' })
+  weight: number;
+
+  @IsOptional()
+  @IsInt({ message: 'Número de goles inválido, debe ser un entero' })
+  goals?: number;
+
+  @IsOptional()
+  @IsInt({ message: 'Número de asistencias inválido, debe ser un entero' })
+  assists?: number;
+}
--- backend/src/modules/players/entities/player.entity.ts
+++ backend/src/modules/players/entities/player.entity.ts
@@ -1,1 +1,35 @@
-export class Player {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { DominantFoot } from '../enums/dominantFoot.enum';
+
+@Entity('players')
+export class Player {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 60 })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  country: string;
+
+  @Column({ type: 'date' })
+  birthdate: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  position: string;
+
+  @Column({ type: 'enum', enum: DominantFoot })
+  dominanFoot: DominantFoot;
+
+  @Column({ type: 'decimal' })
+  height: number;
+
+  @Column({ type: 'decimal' })
+  weight: number;
+
+  @Column({ type: 'int', nullable: true })
+  goals?: number;
+
+  @Column({ type: 'int', nullable: true })
+  assits?: number;
+}
