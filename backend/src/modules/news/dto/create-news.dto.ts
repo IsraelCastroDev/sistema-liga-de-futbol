6ae61d30f869af5ec24988994aff4afb1ab1bd5e
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-10 22:51:41 -0500

    creando dto para crear noticia

--- backend/src/modules/news/dto/create-news.dto.ts
+++ backend/src/modules/news/dto/create-news.dto.ts
@@ -1,1 +1,19 @@
-export class CreateNewsDto {}
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateNewsDto {
+  @IsNotEmpty({ message: 'El título es obligatorio' })
+  @IsString({ message: 'Título inválido' })
+  title: string;
+
+  @IsNotEmpty({ message: 'El contenido es obligatorio' })
+  @IsString({ message: 'Contenido inválido' })
+  content: string;
+
+  @IsNotEmpty({ message: 'El url de la portada es obligatoria' })
+  @IsString({ message: 'Url inválido' })
+  coverImage: string;
+
+  @IsNotEmpty({ message: 'EL usuario es obligatorio' })
+  @IsInt({ message: 'Id de usuario inálido' })
+  userId: number;
+}
