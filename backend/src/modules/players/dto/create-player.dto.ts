d0aaf2358f95fc5b4912fff65823c16ad9e66898
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-10 23:48:42 -0500

    actualizando dto

--- backend/src/modules/players/dto/create-player.dto.ts
+++ backend/src/modules/players/dto/create-player.dto.ts
@@ -4,23 +4,30 @@ import {
   IsNotEmpty,
   IsOptional,
   IsNumber,
+  IsString,
+  IsDateString,
 } from 'class-validator';
 import { DominantFoot } from '../enums/dominantFoot.enum';
+import { PlayerPosition } from '../enums/playerPositions.enum';
 
 export class CreatePlayerDto {
   @IsNotEmpty({ message: 'El nombre del futbolista es obligatorio' })
+  @IsString({ message: 'El nombre del futbolista es inválido' })
   name: string;
 
   @IsNotEmpty({ message: 'El nombre del país es obligatorio' })
+  @IsString({ message: 'El nombre del país es inválido' })
   country: string;
 
   @IsNotEmpty({
     message: 'La fecha de nacimiento del futbolista es obligatoria',
   })
+  @IsDateString({}, { message: 'Fecha de nacimiento inválida' })
   birthdate: string;
 
   @IsNotEmpty({ message: 'La posición del futbolista es obligatoria' })
-  position: string;
+  @IsEnum(PlayerPosition, { message: 'La posición seleccionada no es válida' })
+  position: PlayerPosition;
 
   @IsEnum(DominantFoot, { message: 'Pie dominante inválido' })
   dominantFoot: DominantFoot;
