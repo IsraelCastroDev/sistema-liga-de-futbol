6783908c82a9b945183ef9a9cccfa3a664b91d1a
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-11 12:43:24 -0500

    importando modulos

--- backend/src/modules/players/players.module.ts
+++ backend/src/modules/players/players.module.ts
@@ -3,9 +3,11 @@ import { PlayersService } from './players.service';
 import { PlayersController } from './players.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Player } from './entities/player.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Player])],
+  imports: [TypeOrmModule.forFeature([Player]), JwtModule, UsersModule],
   controllers: [PlayersController],
   providers: [PlayersService],
 })
