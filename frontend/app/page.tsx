dab7cc575c3fc7b16535c2ac27c7f4b7bbbbab8d
IsraelCastroDev <israelcastro.dev@gmail.com>
2025-03-14 14:09:44 -0500

    importando el modal

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/components/header/Header";
+import ClientModal from "@/components/ui/modal/ClientModal";
 
 export default function Home() {
   return (
     <>
       <Header />
       <h1>hola mundo</h1>
+      <ClientModal />
     </>
   );
 }
